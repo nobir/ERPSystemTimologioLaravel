cb725a4ed293997ac833cf8781162aad2ca662c8
Nobir <nobirfreelancer@gmail.com>
2022-07-05 03:39:23 +0600

    [Fixed] side menu not showing

--- resources/views/components/sidemenu.blade.php
+++ resources/views/components/sidemenu.blade.php
@@ -20,19 +20,19 @@ $receptionist_menus = [];
 @endphp
 
 <div class="animate-100 d-md-block list-group mb-3">
-    @if (Session::has('type') && Session::get('type') == 'admin')
+    @if (Session::has('user') && Session::get('user')->type == 1)
         @foreach ($admin_menus as $menu)
             <a href="{{ $menu['path'] }}" class="list-group-item list-group-item-action">{{ $menu['title'] }}</a>
         @endforeach
-    @elseif (Session::has('type') && Session::get('type') == 'manager')
+    @elseif (Session::has('user') && Session::get('user')->type == 2)
         @foreach ($manager_menus as $menu)
             <a href="{{ $menu['path'] }}" class="list-group-item list-group-item-action">{{ $menu['title'] }}</a>
         @endforeach
-    @elseif (Session::has('type') && Session::get('type') == 'employee')
+    @elseif (Session::has('user') && Session::get('user')->type == 3)
         @foreach ($employee_menus as $menu)
             <a href="{{ $menu['path'] }}" class="list-group-item list-group-item-action">{{ $menu['title'] }}</a>
         @endforeach
-    @elseif (Session::has('type') && Session::get('type') == 'receptionist')
+    @elseif (Session::has('user') && Session::get('user')->type == 4)
         @foreach ($receptionist_menus as $menu)
             <a href="{{ $menu['path'] }}" class="list-group-item list-group-item-action">{{ $menu['title'] }}</a>
         @endforeach
