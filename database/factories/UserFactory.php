2c9f0f62f25a772085d7959931e814ef2b7dabfb
Nobir <nobirfreelancer@gmail.com>
2022-07-09 23:40:37 +0600

    [Fixed] wrong input seeding to user type and password encryption

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,7 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use App\Models\Address;
+use Illuminate\Support\Facades\Crypt;
 
 class UserFactory extends Factory
 {
@@ -18,11 +18,11 @@ class UserFactory extends Factory
             'verified' => random_int(0, 1),
             'verify_email' => -1,
             'username' => $this->faker->unique()->userName(),
-            'type' => random_int(-1, 4),
+            'type' => random_int(0, 4),
             'status' => random_int(0, 1),
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => Crypt::encrypt('password'),
             'salary' => $this->faker->randomFloat(2, 0, 100000),
             'hire_date' => $this->faker->dateTimeBetween('-10 years', 'now'),
             'address_id' => $this->faker->unique()->numberBetween(101, 200),
