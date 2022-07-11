79fe9ac2823fa9b24576c64ee094fdb02845325c
Nobir <nobirfreelancer@gmail.com>
2022-07-11 15:09:47 +0600

    [Added] seed a CEO type user for fast login and demostration

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Address;
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use Illuminate\Support\Facades\Crypt;
 
 class UserSeeder extends Seeder
 {
@@ -15,5 +17,30 @@ class UserSeeder extends Seeder
     public function run()
     {
         User::factory()->count(100)->create();
+
+        // Need a user to login
+        $user = new User();
+        $user->verified = 1;
+        $user->verify_email = 0;
+        $user->username = 'nobir';
+        $user->type = 1; // CEO
+        $user->status = 0;
+        $user->name = 'Nobir Hossain';
+        $user->email = 'nobirhossain3360@gmail.com';
+        $user->password = Crypt::encrypt('password');
+        $user->salary = 80000.00;
+        $user->hire_date = now();
+
+        $address = new Address();
+        $address->local_address = 'Uttara';
+        $address->police_station = 'Uttara';
+        $address->city = 'Dhaka';
+        $address->country = 'Bangladesh';
+        $address->zip_code = '1230';
+        $address->save();
+
+        $user->address_id = $address->id;
+        $user->station_id = 10;
+        $user->save();
     }
 }
