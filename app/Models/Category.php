01085f96058dbcdfd6b22cf282ac53d06fb02c7d
Nobir <nobirfreelancer@gmail.com>
2022-08-28 10:01:18 +0600

    [Fixed] category model doesn't have relationship with invoice

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -15,4 +15,9 @@ class Category extends Model
     {
         return $this->hasMany(Inventory::class);
     }
+
+    public function invoices()
+    {
+        return $this->belongsToMany(Invoice::class);
+    }
 }
