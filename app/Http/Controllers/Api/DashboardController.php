ea5ef830796015e3414ecbb966f1471fd29c2a75
Nobir <nobirfreelancer@gmail.com>
2022-08-28 10:02:53 +0600

    [Added] all the common features API for all user (DashboardController)

--- app/Http/Controllers/Api/DashboardController.php
+++ app/Http/Controllers/Api/DashboardController.php
@@ -26,191 +26,196 @@ class DashboardController extends Controller
 
     public function profile(Request $request)
     {
-        $validation = Validator::make(
-            $request->all(),
-            [
-                'auth_id' => 'required',
-            ]
-        );
-
-        $user = User::where('id', $request->auth_id)->with('address', 'region', 'branch', 'permissions')->first();
+        $user = User::where('id', $request->header('auth_id'))->with('address', 'region', 'branch', 'permissions')->first();
         return response()->json($user);
     }
 
-    // public function profileEdit(Request $request)
-    // {
-    //     $user = User::where('id', $request->session()->get('user')->id)->first();
+    public function profileEdit(Request $request)
+    {
+        $user = User::where('id', $request->header('auth_id'))->with("address", "permissions")->first();
 
-    //     $permissions = Permission::all();
+        $permissions = Permission::all();
 
-    //     $user_types = [
-    //         '4' => 'Receptionist',
-    //         '3' => 'Employee',
-    //         '2' => 'Manager',
-    //         '1' => 'CEO',
-    //         // '0' => 'System Admin',
-    //     ];
-    //     return view('dashboard.profileEdit')
-    //         ->with('user', $user)
-    //         ->with('user_types', $user_types)
-    //         ->with('permissions', $permissions);
-    // }
+        $permissions_ids = [];
 
-    // public function profileEditSubmit(Request $request)
-    // {
-    //     $user = User::find($request->session()->get('user')->id);
+        foreach ($user->permissions as $value) {
+            $permissions_ids[] = $value->id;
+        }
 
-    //     $validation_rules = [
-    //         'name' => 'required|min:3',
-    //         'hire_date' => 'required|date|date_format:Y-m-d',
-    //         'salary' => 'required|numeric|min:0',
-    //     ];
+        $user_types = [
+            'CEO',
+            'Region Manager',
+            'Branch Manager',
+            'Employee',
+        ];
+        // 'permission_ids' => $_permissions
+        return response()->json(['user' => $user, 'user_type' => $user_types, 'permissions' => $permissions, 'permission_ids' => $permissions_ids]);
+    }
 
-    //     if ($user->type <= 1) {
-    //         $request->merge([
-    //             'permission_ids' => array_unique(array_filter($request->permission_ids)),
-    //         ]);
+    public function profileEditSubmit(Request $request)
+    {
+        // return response()->json([$request->all()]);
+        $user = User::find($request->header("auth_id"));
 
-    //         $validation_rules = array_merge(
-    //             $validation_rules,
-    //             [
-    //                 // 'type' => 'required|numeric|min:0|max:4',
-    //                 // 'station_id' => 'required|numeric|exists:stations,id',
-    //                 'permission_ids' => 'required|array|min:1|exists:permissions,id',
-    //             ]
-    //         );
-    //     }
+        $validation_rules = [
+            'name' => 'required|min:3',
+            'hire_date' => 'required|date|date_format:Y-m-d',
+            'salary' => 'required|numeric|min:0',
+        ];
 
-    //     // return $user->id;
+        if ($user->type <= 1) {
+            $request->merge([
+                'permission_ids' => array_unique(array_filter($request->permission_ids ?? [])),
+            ]);
 
-    //     $this->validate($request, $validation_rules);
+            $validation_rules = array_merge(
+                $validation_rules,
+                [
+                    // 'type' => 'required|numeric|min:0|max:4',
+                    // 'station_id' => 'required|numeric|exists:stations,id',
+                    'permission_ids' => 'required|array|min:1|exists:permissions,id',
+                ]
+            );
+        }
 
-    //     if ($user->type <= 1) {
+        // return $user->id;
 
-    //         foreach ($request->permission_ids as $permission) {
-    //             if (!Permission::find($permission)) {
-    //                 $request->session()->flash('error_message', 'Permission not found.');
+        $validation = Validator::make($request->all(), $validation_rules);
 
-    //                 return redirect()->back();
-    //             }
-    //         }
+        if ($validation->fails()) {
+            return response()->json(["error_list" => $validation->errors()], 422);
+        }
 
-    //         // $user->type = $request->type;
-    //         $user->hire_date = $request->hire_date;
-    //         $user->salary = $request->salary;
-    //         $user->permissions()->detach();
-    //         // return back();
-    //         foreach ($request->permission_ids as $permission) {
-    //             $user->permissions()->attach($permission);
-    //         }
-    //     }
+        if ($user->type <= 1) {
+            $permissions_ids = $request->permission_ids && is_array($request->permission_ids) ? $request->permission_ids : [];
+            foreach ($permissions_ids as $permission) {
+                if (!Permission::find($permission)) {
+                    // $request->session()->flash('error_message', 'Permission not found.');
+                    return response()->json(["error_message" => 'Permission not found'], 406);
 
-    //     // return $request->input();
+                    // return redirect()->back();
+                }
+            }
 
-    //     $user->name = $request->name;
-    //     $user->address->local_address = $request->local_address;
-    //     $user->address->police_station = $request->police_station;
-    //     $user->address->city = $request->city;
-    //     $user->address->country = $request->country;
-    //     $user->address->zip_code = $request->zip_code;
-    //     $user->address->update();
-    //     $user->update();
+            // $user->type = $request->type;
+            $user->hire_date = $request->hire_date;
+            $user->salary = $request->salary;
+            $user->permissions()->detach();
+            // return back();
+            foreach ($permissions_ids as $permission) {
+                $user->permissions()->attach($permission);
+            }
+        }
 
-    //     $request->session()->remove('user');
-    //     $request->session()->put('user', $user);
+        // return $request->input();
 
-    //     $request->session()->flash('success_message', "updated successfully.");
+        $user->name = $request->name;
+        $user->address->local_address = $request->address["local_address"];
+        $user->address->police_station = $request->address["police_station"];
+        $user->address->city = $request->address["city"];
+        $user->address->country = $request->address["country"];
+        $user->address->zip_code = $request->address["zip_code"];
+        $user->address->update();
+        $user->update();
 
-    //     return redirect()->back();
-    // }
+        // $request->session()->remove('user');
+        // $request->session()->put('user', $user);
+
+        // $request->session()->flash('success_message', "Updated successfully.");
+        return response()->json(["success_message" => 'Updated successfully']);
+
+        // return redirect()->back();
+    }
 
     // public function profilePicture(Request $request)
     // {
     //     return view('dashboard.profilePicture');
     // }
 
-    // public function profilePictureSubmit(Request $request)
-    // {
-    //     $this->validate(
-    //         $request,
-    //         [
-    //             'avatar' => 'required|max:2048|image|file',
-    //         ]
-    //     );
+    public function profilePictureSubmit(Request $request)
+    {
+        $validation = Validator::make(
+            $request->all(),
+            [
+                'avatar' => 'required|max:2048|image|file',
+            ]
+        );
 
-    //     $user = User::find($request->session()->get('user')->id);
+        if ($validation->fails()) {
+            return response()->json(["error_list" => $validation->errors()], 422);
+        }
 
-    //     if (!$user) {
-    //         $request->session()->flash('error_message', 'User not found.');
-    //         return redirect()->back();
-    //     }
+        $user = User::find($request->header("auth_id"));
 
-    //     // return public_path();
+        if (!$user) {
+            return response()->json(["error_message" => 'User not found'], 406);
+        }
 
-    //     $path = $request->file('avatar')->store('public/avatars');
+        // return public_path();
 
-    //     if (!$path) {
-    //         $request->session()->flash('error_message', 'Error uploading avatar.');
-    //         return redirect()->back();
-    //     }
+        $path = $request->file('avatar')->store('public/avatars');
 
-    //     $user->avatar = str_replace('public', 'storage', $path);
-    //     $user->update();
+        if (!$path) {
+            return response()->json(["error_message" => 'Error uploading avatar'], 406);
+        }
 
-    //     $request->session()->remove('user');
-    //     $request->session()->put('user', $user);
+        $user->avatar = str_replace('public', 'storage', $path);
+        $user->update();
 
-    //     $request->session()->flash('success_message', "Uploaded avatar successfully.");
-    //     return redirect()->back();
-    // }
+        // $request->session()->remove('user');
+        // $request->session()->put('user', $user);
+
+        return response()->json(["success_message" => 'Uploaded avatar successfully']);
+    }
 
     // public function changePassword(Request $request)
     // {
     //     return view('dashboard.changePassword');
     // }
 
-    // public function changePasswordSubmit(Request $request)
-    // {
-    //     // dd($request->all());
+    public function changePasswordSubmit(Request $request)
+    {
+        // dd($request->all());
 
-    //     /**
-    //      * reqular expression
-    //      * @link https://regexr.com/6pguf
-    //      */
+        /**
+         * reqular expression
+         * @link https://regexr.com/6pguf
+         */
 
-    //     $this->validate(
-    //         $request,
-    //         [
-    //             'currentpass' => 'required',
-    //             'newpass' => 'required|min:8|regex:/^(?=.*[\!\@\#\$\%\^\&\*\(\)])(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/',
-    //             'retypepass' => 'required|same:newpass',
-    //         ],
-    //         [
-    //             'newpass.regex' => "Password must contain at least one special character, one uppercase letter, one lowercase letter and one digit.",
-    //         ]
-    //     );
+        $validation = Validator::make(
+            $request->all(),
+            [
+                'currentpass' => 'required',
+                'newpass' => 'required|min:8|regex:/^(?=.*[\!\@\#\$\%\^\&\*\(\)])(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/',
+                'retypepass' => 'required|same:newpass',
+            ],
+            [
+                'newpass.regex' => "Password must contain at least one special character, one uppercase letter, one lowercase letter and one digit.",
+            ]
+        );
 
-    //     $user = User::find($request->session()->get('user')->id);
+        if ($validation->fails()) {
+            return response()->json(["error_list" => $validation->errors()], 422);
+        }
 
-    //     if (!$user) {
-    //         $request->session()->flash('error_message', 'User not found.');
-    //         return redirect()->back();
-    //     }
+        $user = User::find($request->header('auth_id'));
 
-    //     $check_current_pass = Crypt::decrypt($user->password) . "" === $request->currentpass . "";
+        if (!$user) {
+            return response()->json(["error_message" => 'User not found'], 406);
+        }
 
-    //     if (!$check_current_pass) {
-    //         $request->session()->flash('error_message', 'Current password is incorrect.');
-    //         return redirect()->back();
-    //     }
+        $check_current_pass = Crypt::decrypt($user->password) . "" === $request->currentpass . "";
 
-    //     $user->password = Crypt::encrypt($request->newpass);
-    //     $user->update();
+        if (!$check_current_pass) {
+            return response()->json(["error_message" => 'Current password is incorrect'], 406);
+        }
 
-    //     $request->session()->remove('user');
-    //     $request->session()->put('user', $user);
+        $user->password = Crypt::encrypt($request->newpass);
+        $user->update();
 
-    //     $request->session()->flash('success_message', "Password changed successfully.");
-    //     return redirect()->back();
-    // }
+        // $request->session()->remove('user');
+        // $request->session()->put('user', $user);
+
+        return response()->json(["success_message" => 'Password changed successfully']);
+    }
 }
